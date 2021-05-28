d7a1432ac0314d2ccbce0cf2e4b5c01c37efc169
JeongkyuKim-git <jkkim@mme.dongguk.edu>
2021-05-28 22:26:06 +0900

    Update Test_file.py

--- Anchoring_method/Test_file.py
+++ Anchoring_method/Test_file.py
@@ -1,10 +1,15 @@
-with open('Req_01.fasta') as f1:
+def header_and_seq(file_name):
+    with open(file_name) as f1:
         data_storege = ""
         for line in f1:
-                if line.startswith('>'):
-                        header=line
-                        print(header)
-                else:
-                        data_storege += line.strip()
-return hedear, data_storege
+            if line.startswith('>'):
+                header = line
+                print(header)
+            else:
+                data_storege += line.strip()
+        print(data_storege)
+    return header, data_storege
 
+
+text = 'Req_01.fasta'
+header_and_seq(text)
